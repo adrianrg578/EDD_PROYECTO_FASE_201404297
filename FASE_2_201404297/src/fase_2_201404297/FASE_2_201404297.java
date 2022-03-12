36950b545a144b84a68faa007e74c089bffbc19d
adrianrg578 <201404297@ingenieria.usac.edu.gt>
2022-03-12 14:34:08 -0600

    ventanas
    Creacion de login y pagina principal

--- FASE_2_201404297/src/fase_2_201404297/FASE_2_201404297.java
+++ FASE_2_201404297/src/fase_2_201404297/FASE_2_201404297.java
@@ -15,6 +15,8 @@ public class FASE_2_201404297 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login log = new Login();
+        log.setVisible(true);
     }
     
 }
