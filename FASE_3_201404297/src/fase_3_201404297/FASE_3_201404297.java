65821854d22008233969606ea522fa7649be0618
adrianrg578 <201404297@ingenieria.usac.edu.gt>
2022-04-28 17:43:04 -0600

    interfaz grafica
    Creacion de la interfaz grafica basica para iniciar

---
+++ FASE_3_201404297/src/fase_3_201404297/FASE_3_201404297.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fase_3_201404297;
+
+/**
+ *
+ * @author adria
+ */
+public class FASE_3_201404297 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
